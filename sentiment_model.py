fcf4bc1bfa2ddbbd36f27f8486472965f8d19a01
Aleksandr Lebedev <mr.alexandrlebedev@mail.ru>
2022-05-20 23:02:59 +0300

    add streamlit

--- sentiment_model.py
+++ sentiment_model.py
@@ -1,5 +1,29 @@
+import streamlit as st
 from transformers import pipeline
+
+st.set_page_config(
+    page_title="Sentimental_ru",
+    page_icon="🤗",
+)
 
 classifier = pipeline("sentiment-analysis", "blanchefort/rubert-base-cased-sentiment")
 
-print(classifier("Я обожаю инженерию машинного обучения!"))
+text = st.text_area(
+            "Введите ваш текст здесь (максимум 500 слов)",
+            height=510,
+        )
+MAX_WORDS = 500
+import re
+
+res = len(re.findall(r"\w+", text))
+if res > MAX_WORDS:
+    st.warning(
+        "⚠️ Ваш текст содержит "
+        + str(res)
+        + " слов."
+        + " толькое первые 500 слов будут проанализированы. Следите за новостями, скоро лимит будет увеличен! 😊"
+    )
+
+    text = text[:MAX_WORDS]
+
+classifier(text)
